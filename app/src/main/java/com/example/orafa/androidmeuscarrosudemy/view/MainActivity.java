30f2d25d4433f3592fb0429a96097f5af7d56c23
Rafa Lima <paracafa@gmail.com>
2017-12-11 20:35:27 -0300

    Aula 21 - Colocando Icon na Action da Activity Main em vez do nome do App

--- app/src/main/java/com/example/orafa/androidmeuscarrosudemy/view/MainActivity.java
+++ app/src/main/java/com/example/orafa/androidmeuscarrosudemy/view/MainActivity.java
@@ -33,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
         this.mContext = this;
 
         CarMock carMock = new CarMock();
